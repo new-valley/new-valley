875d8a4af0f81f1ada6b3e495553e20d18b2b53f
Erik Perillo <erik.perillo@gmail.com>
2018-11-10 17:18:53 -0200

    fix on run_server.sh script

--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-[[ -z $MEMEDATA_ENV ]] && app_env='development' || app_env=$MEMEDATA_ENV
-[[ -z $MEMEDATA_HOST ]] && host='localhost' || host=$MEMEDATA_HOST
-[[ -z $MEMEDATA_PORT ]] && port=5000 || port=$MEMEDATA_PORT
+[[ -z $NEWVALLEY_ENV ]] && app_env='development' || app_env=$NEWVALLEY_ENV
+[[ -z $NEWVALLEY_HOST ]] && host='localhost' || host=$NEWVALLEY_HOST
+[[ -z $NEWVALLEY_PORT ]] && port=5000 || port=$NEWVALLEY_PORT
 export FLASK_APP=nv.app:get_app
 export FLASK_ENV=$app_env
 flask run --host=$host --port=$port $@
