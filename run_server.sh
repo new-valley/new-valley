2b76670955d19a593945cf2726d71f6ef74cfbc3
Erik Perillo <erik.perillo@gmail.com>
2018-11-14 23:58:09 -0200

    support for WSGI production server added

--- run_server.sh
+++ run_server.sh
@@ -3,8 +3,13 @@
 [[ -z $NEWVALLEY_ENV ]] && app_env='development' || app_env=$NEWVALLEY_ENV
 [[ -z $NEWVALLEY_HOST ]] && host='localhost' || host=$NEWVALLEY_HOST
 [[ -z $NEWVALLEY_PORT ]] && port=5000 || port=$NEWVALLEY_PORT
+[[ -z $NEWVALLEY_N_WORKERS ]] && n_workers=1 || n_workers=$NEWVALLEY_N_WORKERS
 
 export FLASK_APP=nv.app:get_app
 export FLASK_ENV=$app_env
 
-flask run --host=$host --with-threads --port=$port $@
+if [[ "$app_env" == 'development' ]]; then
+    flask run --host=$host --with-threads --port=$port $@
+else
+    gunicorn -w $n_workers 'nv.app:get_app()'
+fi
