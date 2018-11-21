22cfcc49224c6938c1c78490fee2fa1286f6c576
Erik Perillo <erik.perillo@gmail.com>
2018-11-20 23:51:32 -0200

    put CORS in extensions module

--- nv/app.py
+++ nv/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from flask_cors import CORS
 
-from nv.extensions import db, api, jwt
+from nv.extensions import db, api, jwt, cors
 from nv import metaconfig
 from nv.errors import register_handlers
 
@@ -18,13 +17,12 @@ def get_app(conf_obj=None):
     register_extensions(app)
     #error handlers
     register_error_handlers(app)
-    #enable CORS
-    CORS(app)
     return app
 
 def register_extensions(app):
     db.init_app(app)
     jwt.init_app(app)
+    cors.init_app(app)
     api.init_app(app)
 
 def register_error_handlers(app):
--- nv/extensions.py
+++ nv/extensions.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 jwt = JWTManager()
+
+cors = CORS()
 
 from nv.resources import (
     auth,
